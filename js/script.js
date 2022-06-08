2a54c209a3a5f37d8c7f589ed548d70252d03fda
Basma Abd Elzaher <basmaabdelzaher@gmail.com>
2022-06-08 11:50:11 +0200

    side-menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 // $(document).ready(function(){
   
 // });
+
+function toggleSidebar () {
+    $('#sideMenuWrapper').toggleClass('show');
+    if(!$('#sideMenuWrapper').hasClass('show')){
+        $('.toggler').removeClass('show');
+        $('.items').slideUp();
+    }
+}
+
+function showItems(toggler, items) {
+    $(toggler).toggleClass('show');
+    $('#'+ items).slideToggle();
+}
